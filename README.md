e83d0861ee937882feeff2199c9f18283c31649a
eightdoor <851708184@qq.com>
2020-12-24 16:18:24 +0800

    fix

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # taro基础项目
+## 构建
+- react-hooks、redux、typescript
 ## 启动
 - h5 `npm run dev:h5`
+## 依赖
+- 组件库 taro-ui (版本3.0.0-alpha.7)
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,79 +1,29 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
+import { useDispatch, useSelector} from 'react-redux'
 import { View, Button, Text } from '@tarojs/components'
+import {AtButton} from 'taro-ui';
 
 import { add, minus, asyncAdd } from '../../actions/counter'
 
 import './index.less'
 
-// #region 书写注意
-//
-// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
-// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
-// 这样才能完成类型检查和 IDE 的自动提示
-// 使用函数模式则无此限制
-// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
-//
-// #endregion
-
-type PageStateProps = {
-  counter: {
-    num: number
-  }
-}
-
-type PageDispatchProps = {
-  add: () => void
-  dec: () => void
-  asyncAdd: () => any
-}
-
-type PageOwnProps = {}
-
-type PageState = {}
-
-type IProps = PageStateProps & PageDispatchProps & PageOwnProps
-
-interface Index {
-  props: IProps;
-}
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
-class Index extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
-  }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
+const Index = ()=>{
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+  return (
+    <>
       <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
+        <Button className='add_btn' onClick={()=> dispatch(add())}>+</Button>
+        <Button className='dec_btn' onClick={()=>dispatch(minus())}>-</Button>
+        <Button className='dec_btn' onClick={()=>dispatch(asyncAdd())}>async</Button>
+        <View><Text>{counter.num}</Text></View>
         <View><Text>Hello, World</Text></View>
+        <View>
+          <AtButton type='primary'>测试</AtButton>
+        </View>
       </View>
-    )
-  }
+    </>
+  )
 }
-
 export default Index
 
